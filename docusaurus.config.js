20b357f71294f5ce8c7306be783d01dc88d6eae9
Aryan Singh <aryan.iitgn@gmail.com>
2023-03-13 17:53:58 +0530

    stashing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
 
-          editUrl: 'https://dev.azure.com/skypointgroup/skypoint/_git/spc-documentation',
+          editUrl: 'https://github.com/AryanSingh/doc_poc',
           // sidebarPath: 'sidebars.js',
         },
         blog: false,
