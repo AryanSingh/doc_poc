9b0e03d5d69e3b7f225d48f7508e5fc9c06bfd40
Aryan Singh <aryan.iitgn@gmail.com>
2023-03-17 20:57:42 +0530

    fixed edit this doc link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
 
-          editUrl: 'https://github.com/AryanSingh/doc_poc',
+          editUrl: 'https://github.com/AryanSingh/doc_poc/blob/master',
           // sidebarPath: 'sidebars.js',
         },
         blog: false,
